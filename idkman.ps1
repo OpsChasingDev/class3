286a1254273e5265f610bd440d3b0099dc69771c
Robert Stapleton <robert@stapleton-family.net>
2024-06-05 13:54:46 -0400

    writing info of manual services to host

--- idkman.ps1
+++ idkman.ps1
@@ -1,1 +1,7 @@
-Get-Service | Where-Object {$_.StartType -eq "Manual"}
+$services = Get-Service | Where-Object {$_.StartType -eq "Manual"}
+
+foreach ($service in $services) {
+    $serviceName = $service.Name
+    $serviceStatus = $service.Status
+    Write-Host "$serviceName is $serviceStatus"
+}
